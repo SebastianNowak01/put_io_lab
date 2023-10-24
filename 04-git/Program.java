b74a72afd0e4ab4b9f46b9b0ebe4a17f5c00bf73
sebnow91 <sebnow91@gmail.com>
2023-10-24 10:26:59 +0200

    Pierwsza zmiana

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,7 +1,9 @@
+// This is HelloWorld class
 class HelloWorld {
     
     public static void main(String[] args) {
 	
         System.out.println("Hello, World! You are beatiful!"); 
     }
+    
 }
